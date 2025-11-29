43f3dbb6e3604641d28113de7502c6824265d886
David Yu <dmancyu@gmail.com>
2025-11-29 14:19:39 +0800

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Hospital Management System",
+    "author": "David Yu",
+    "license": "LGPL-3",
+    "version": "18.0.1.1",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/patient_views.xml",
+        "views/menu.xml",
+    ]
+}
